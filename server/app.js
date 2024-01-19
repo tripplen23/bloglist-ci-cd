7767c14a0b96a397b348e0a6cda2f56a87840c77
tripplen63 <tripplen63@gmail.com>
2024-01-19 04:07:34 +0200

    update health check

--- server/app.js
+++ server/app.js
@@ -39,6 +39,8 @@ if (process.env.NODE_ENV === 'test') {
   app.use('/api/testing', testingRouter)
 }
 
+app.get('/health', (req, res) => res.send('ok'))
+
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
